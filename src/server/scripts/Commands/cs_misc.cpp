0972f8085cc2f9cfd3f785957fffa655d524929f
The Yeti <matt@theyeticave.net>
2024-10-10 07:53:55 -0700

    Just use %u instead of casting to string

--- src/server/scripts/Commands/cs_misc.cpp
+++ src/server/scripts/Commands/cs_misc.cpp
@@ -2488,7 +2488,7 @@ public:
         if (!*args) {
             if (QueryResult result = CharacterDatabase.Query("SELECT xprate FROM character_xprate")) {
                 uint32 currate = result->Fetch()->GetUInt32();
-                handler->PSendSysMessage(LANG_CHAR_XPRATE, std::to_string(currate));
+                handler->PSendSysMessage(LANG_CHAR_XPRATE, currate);
                 handler->SetSentErrorMessage(true);
                 return true;
             }
